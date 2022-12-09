e54a5115199b25b45472054103f5968d3a6617c6
root <aduku.lilian@gmail.com>
2022-12-09 04:39:05 -0800

    adde task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+
+/* main - Entry point,Description: prints "Programming is like building a multilingual puzzle"*/
+
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
