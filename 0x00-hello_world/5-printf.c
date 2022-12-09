501f2950f54ddd7f96ad3fef4ee145fab30da8dc
root <aduku.lilian@gmail.com>
2022-12-09 05:41:07 -0800

    adde task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ * Description: prints "Programming is like building a multilingual puzzle
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
