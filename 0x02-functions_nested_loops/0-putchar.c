0fe5025d5bb6314e730c07afb7f478ae87dff954
root <aduku.lilian@gmail.com>
2022-12-13 02:26:14 -0800

    added task0 of unctions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/** 
+ * Main- entry point
+ *
+ * Description -the function will print a string
+ *
+ * return - a string
+ */
+
+int main(void)
+{
+	printf("%d\n","/_putchar");
+	return 0;
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ this project will teach me abot function in c
